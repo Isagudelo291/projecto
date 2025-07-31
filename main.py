0c3c4ca83feef01672c7805e579cd7a9e28c9664
isagudelo291 <isabelagudelo291@gmail.com>
2025-07-30 21:26:32 -0500

    se guardaron las stopwords en el archivo local c

---
+++ main.py
@@ -0,0 +1,8 @@
+#importar la libreria nltk
+import nltk 
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# definir la ruta donde se almacenarán los datos descargados de nltk
+nltk.data.path.append('C:/Users/isabe/AppData/Roaming/nltk_data')
